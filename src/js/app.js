5229bc7a8c268e48c066f1923766e6c41524caf8
Wojtek Nowak <billnewmann@gmail.com>
2023-11-30 23:21:32 +0000

    start refacoring calc function

--- src/js/app.js
+++ src/js/app.js
@@ -4,18 +4,20 @@ const inputSqM = document.getElementById("inputSqM");
 const inputSqFt = document.getElementById("inputSqFt");
 
 const calcArea = function (unit, area) {
-  const result = unit == "sqM" ? area * 10.7639 : area / 10.7639;
+  const result = unit == "inputSqM" ? area * 10.7639 : area / 10.7639;
   console.log(typeof result);
   return Number.parseFloat(result).toFixed(2);
   //return result.toFixed(2);
 };
 
 inputSqM.addEventListener("keyup", function (e) {
+  e.preventDefault;
+  console.log(e.target);
   console.log("m2");
-  inputSqFt.value = calcArea("sqM", inputSqM.value);
+  inputSqFt.value = calcArea(e.target.id, inputSqM.value);
 });
 
-inputSqFt.addEventListener("keyup", function (e) {
-  console.log("sqft");
-  inputSqM.value = calcArea(false, inputSqFt.value);
-});
+// inputSqFt.addEventListener("keyup", function (e) {
+//   console.log("sqft");
+//   inputSqM.value = calcArea(false, inputSqFt.value);
+// });
