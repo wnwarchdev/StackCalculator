55c8320902c743753ff9f0b118e60c3089b60777
Wojtek Nowak <billnewmann@gmail.com>
2023-12-03 08:53:12 +0000

    test adding multiple level calculators

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 `use strict`;
 
-const inputSqM = document.getElementById("inputSqM");
-const inputSqFt = document.getElementById("inputSqFt");
+// const inputSqM = document.getElementById("inputSqM");
+// const inputSqFt = document.getElementById("inputSqFt");
 const calculator = document.getElementById("calculator");
 
 const calcArea = function (unit, area) {
@@ -9,14 +9,65 @@ const calcArea = function (unit, area) {
   return Number.parseFloat(result).toFixed(2);
 };
 
-calculator.addEventListener("keyup", function (e) {
-  e.preventDefault;
-  if (e.target.tagName == "INPUT") {
-    console.log(e.target);
-    e.target.id == `inputSqM`
-      ? (inputSqFt.value = calcArea(e.target.id, inputSqM.value))
-      : (inputSqM.value = calcArea(e.target.id, inputSqFt.value));
-  } else {
-    console.log("not input");
-  }
-});
+const levelDiv = `<div class="level">
+<p>
+  m2
+  <input
+    id="inputSqM"
+    min="0"
+    type="number"
+    pattern="^\d*(\.\d{0,2})?$"
+    onpaste="return false;"
+    ondrop="return false;"
+    autocomplete="off"
+  />
+</p>
+<p>
+  sqft
+  <input
+    id="inputSqFt"
+    min="0"
+    type="number"
+    pattern="^\d*(\.\d{0,2})?$"
+    onpaste="return false;"
+    ondrop="return false;"
+    autocomplete="off"
+  />
+</p>
+`;
+
+const addLevel = function () {
+  calculator.insertAdjacentHTML("afterbegin", levelDiv);
+  const inputSqM = document.getElementById("inputSqM");
+  const inputSqFt = document.getElementById("inputSqFt");
+  calculator.addEventListener("keyup", function (e) {
+    e.preventDefault;
+    if (e.target.tagName == "INPUT") {
+      console.log(e.target);
+      e.target.id == `inputSqM`
+        ? (inputSqFt.value = calcArea(e.target.id, inputSqM.value))
+        : (inputSqM.value = calcArea(e.target.id, inputSqFt.value));
+    } else {
+      console.log("not input");
+    }
+  });
+};
+
+addLevel();
+addLevel();
+addLevel();
+addLevel();
+addLevel();
+addLevel();
+
+// calculator.addEventListener("keyup", function (e) {
+//   e.preventDefault;
+//   if (e.target.tagName == "INPUT") {
+//     console.log(e.target);
+//     e.target.id == `inputSqM`
+//       ? (inputSqFt.value = calcArea(e.target.id, inputSqM.value))
+//       : (inputSqM.value = calcArea(e.target.id, inputSqFt.value));
+//   } else {
+//     console.log("not input");
+//   }
+// });
